a496c56a6fc2e52ead2f13e833cc63bddee88bc7
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-17 17:08:43 -0500

    finished add post feature

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,3 +8,20 @@ get '/category/:id' do
   @category = Category.find(params[:id])
   erb :posts
 end
+
+get '/category/:category_id/post/:post_id' do
+  @listing = Post.find(params[:post_id])
+  erb :listing
+end
+
+get '/new' do
+  @categories = Category.all
+  erb :new
+end
+
+
+post '/new' do
+  category = Category.find(params[:category]) 
+  @listing = category.posts.create(params[:post])
+  erb :listing
+end
